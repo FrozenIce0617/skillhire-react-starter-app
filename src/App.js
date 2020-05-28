485b359bfe16e63ea9da1b99acca014de8c432aa
FrozenIce0617 <prosdev087@gmail.com>
2020-05-28 14:50:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import { Typography } from '@material-ui/core'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
 import Header from './components/Header'
+import TodoList from './components/TodoList'
+import SubmitForm from './components/SubmitForm'
 import appStyle from './App.style'
 
 const useStyles = makeStyles(appStyle)
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { content: 'Task 1', isDone: false },
+    { content: 'Task 2', isDone: false },
+  ])
   const classes = useStyles()
+
+  const onToggle = (index) => {
+    const newTodos = todos.map((todo, idx) => {
+      if (index === idx) return { ...todo, isDone: !todo.isDone }
+      return todo
+    })
+
+    setTodos(newTodos)
+  }
+
+  const onHandleSubmit = (newTodo) => {
+    setTodos([...todos, { content: newTodo, isDone: false }])
+  }
 
   return (
     <div className={classes.container}>
       <div className={classes.card}>
-        <Header numTodos={3} />
-        <Typography variant='h4'>Welcome to Skillhire</Typography>
+        <Header numTodos={todos.filter((item) => !item.isDone).length} />
+        <TodoList todos={todos} onToggle={onToggle} />
+        <SubmitForm onSubmit={onHandleSubmit} />
       </div>
     </div>
   )
